292587142603f16da1a4d33fc56030ec3ffb0605
Lars Erik Wik <lars.erik.wik@northern.tech>
2021-10-25 16:22:31 +0200

    GH Action verifying that contributor changed both version and commit
    Ticket: CFE-3800
    Changelog: None
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

---
+++ .github/workflows/compare_index_changes.py
@@ -0,0 +1,50 @@
+import sys
+import json
+from collections import OrderedDict
+
+
+def main():
+    try:
+        with open("index.json", "r") as f:
+            new_index = json.loads(f.read(), object_pairs_hook=OrderedDict)
+        with open("index.old.json", "r") as f:
+            old_index = json.loads(f.read(), object_pairs_hook=OrderedDict)
+        compare_index_changes(new_index, old_index)
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+    sys.exit(0)
+
+
+def compare_index_changes(new_index, old_index):
+    assert "modules" in new_index
+    new_modules = new_index["modules"]
+
+    assert "modules" in old_index
+    old_modules = old_index["modules"]
+
+    for module_name in new_modules:
+        if module_name not in old_modules:
+            continue
+
+        new_module = new_modules[module_name]
+        old_module = old_modules[module_name]
+
+        # Handled by another GH Action
+        if (("version", "commit") not in new_module or
+            ("version", "commit") not in old_module):
+            continue
+
+        if new_module["version"] != old_module["version"]:
+            if new_module["commit"] == old_module["commit"]:
+                print("Error: Attribute 'version' was changed, but not 'commit'")
+                sys.exit(1)
+
+        if new_module["commit"] != old_module["commit"]:
+            if new_module["version"] == old_module["commit"]:
+                print("Error: Attribute 'commit' was changed but not 'version'")
+                sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
